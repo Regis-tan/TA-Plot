570fb6c39b3ee4e650cacb4b7ce5aa5f05fd9d81
Regis-tan <144893732+Regis-tan@users.noreply.github.com>
2023-11-29 16:40:14 +0700

    w

---
+++ TA Lab 9.py
@@ -0,0 +1,18 @@
+#This is code to plot the number of stabbing crimes in london from the time period 2015-2022.
+#In 2020, there was a sharp drop in stabbing crimes in london due to the covid restrictions.
+#Data gotten from: https://www.statista.com/statistics/864736/knife-crime-in-london/
+
+import matplotlib.pyplot as plt
+
+#data
+x = [2015,2016,2017,2018,2019,2020,2021,2022]
+y = [9752,12077,14731,14902,15928,10150,11122,12786]
+
+#labels
+plt.xlabel("Year")
+plt.ylabel("Stabbings")
+plt.title("Stabbings in London 2015-2022")
+
+plt.grid(True)
+plt.plot(x,y)
+plt.show()
